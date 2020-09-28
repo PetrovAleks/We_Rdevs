089a4771ed373395aa92d0c9297975cd89724fb1
Petrov Aleksandr <rpm10b@gmail.com>
2020-09-28 09:46:20 +0300

    Updates

---
+++ precache-manifest.c1b6d8c912cb1d49a338be80e0d8772a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0938d2549f7ea8259ee06d4672768b9",
+    "url": "/We_Rdevs/index.html"
+  },
+  {
+    "revision": "98534cce78e3fb305228",
+    "url": "/We_Rdevs/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "f63d85a1af4936353c6c",
+    "url": "/We_Rdevs/static/css/main.d34691a8.chunk.css"
+  },
+  {
+    "revision": "98534cce78e3fb305228",
+    "url": "/We_Rdevs/static/js/2.f7f3d15c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/We_Rdevs/static/js/2.f7f3d15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63d85a1af4936353c6c",
+    "url": "/We_Rdevs/static/js/main.f4f0fde2.chunk.js"
+  },
+  {
+    "revision": "dc209367270fdd97790e",
+    "url": "/We_Rdevs/static/js/runtime-main.73591d39.js"
+  },
+  {
+    "revision": "e6902127f28740067011708b60d646cc",
+    "url": "/We_Rdevs/static/media/hero.e6902127.png"
+  },
+  {
+    "revision": "82e325d5bc492b9994102e51c4969fce",
+    "url": "/We_Rdevs/static/media/hero@2x.82e325d5.png"
+  },
+  {
+    "revision": "197a14cb2fd56f4c28fabc94359393f9",
+    "url": "/We_Rdevs/static/media/hero@3x.197a14cb.png"
+  },
+  {
+    "revision": "f899aafb32913385b7a6921397d42388",
+    "url": "/We_Rdevs/static/media/logo.f899aafb.svg"
+  }
+]);
